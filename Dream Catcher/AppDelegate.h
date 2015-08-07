c9f806385d87f42df4ad3cf3e2d557427ce88655
Kellen Pierson <kellenpierson@gmail.com>
2015-08-07 11:45:45 -0700

    Initial Commit

---
+++ Dream Catcher/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dream Catcher
+//
+//  Created by Kellen Pierson on 8/7/15.
+//  Copyright (c) 2015 Kellen Pierson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Dream Catcher/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dream Catcher
+//
+//  Created by Kellen Pierson on 8/7/15.
+//  Copyright (c) 2015 Kellen Pierson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
